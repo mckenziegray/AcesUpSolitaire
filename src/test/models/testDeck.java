2b49be01f032dec44b22c67f5d0bb0924cdf5a66
Kevin Koos <kooske@oregonstate.edu>
2017-12-01 14:41:40 -0800

    Update testDeck.java

--- src/test/models/testDeck.java
+++ src/test/models/testDeck.java
@@ -1,16 +1,1 @@
-package models;
 
-import org.junit.Test;
-
-public class deckTest {
-    @Test
-    public void testDraw(){
-        Deck deck = new Deck();
-        deck.takeTopCard();
-    }
-
-    @Test
-    public void testBuild(){
-        Deck deck = new Deck();
-    }
-}
