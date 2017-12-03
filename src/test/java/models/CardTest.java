0f464e9bfd2f8151070c81110867d6b36ffb56b1
McKenzie <graymc@oregonstate.edu>
2017-12-03 12:05:56 -0800

    Removed isAce test

--- src/test/java/models/CardTest.java
+++ src/test/java/models/CardTest.java
@@ -22,17 +22,6 @@ public class CardTest {
     }
 
     @Test
-    public void testIsAce() {
-        assertEquals(false, card.isAce());
-        Card card2 = new Card(13, Suit.Spades);
-        assertEquals(false, card2.isAce());
-        Card ace1 = new Card(14, Suit.Spades);
-        assertEquals(true, ace1.isAce());
-        Card ace2 = new Card(13, Suit.Espadas);
-        assertEquals(true, ace2.isAce());
-    }
-
-    @Test
     public void testToString(){
         String str = card.toString();
         assertEquals("5Spades",str);
